43ccd39a55a9f1bbc29929abb02f38c0e67afb5c
Victor Loh <vic94loh@hotmail.com>
2021-06-27 10:20:33 +0800

    moved main to src/bin/went

--- src/bin/went/main.rs
+++ src/bin/went/main.rs
@@ -6,7 +6,9 @@ fn main() {
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("failed to read line");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
 
     println!("You guessed: {}", guess);
 }
